9ad9236354d27a3234bb13ba31d8eb75ab38d358
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-09-11 13:19:32 +0300

    Update dependencies in pubspec.lock and modify main.dart to set the home view to MorningView and change the app title to 'Minders App'.

---
+++ lib/feature/creating_habit/views/morning_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MorningView extends StatelessWidget {
+  const MorningView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF8E97FD),
+    );
+  }
+}
