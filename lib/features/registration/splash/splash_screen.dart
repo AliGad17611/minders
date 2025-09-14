aec3f5c861bd8ecc6822d4bad19d3b27082289d0
ELDemy <mahmoudeldemerdash5@gmail.com>
2025-09-14 18:22:23 +0300

    emove Firebase initialization from main to splash implement Firebase setup in SplashScreen with authentication check and navigation to appropriate views based on user status.

--- lib/features/registration/splash/splash_screen.dart
+++ lib/features/registration/splash/splash_screen.dart
@@ -1,6 +1,12 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:minders/core/utils/assets/app_images.dart';
 import 'package:minders/core/utils/themes/app_colors.dart';
+import 'package:minders/features/creating_habit/views/morning_view.dart';
+import 'package:minders/firebase_options.dart';
 
 import 'get_started_screen.dart';
 
@@ -15,14 +21,12 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      if (mounted) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const GetStartedScreen()),
-        );
-      }
-    });
+    initializeApp();
+  }
+
+  initializeApp() async {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    Future.delayed(const Duration(seconds: 2), checkAuth);
   }
 
   @override
@@ -46,5 +50,21 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
       ),
     );
+  }
+
+  void checkAuth() async {
+    final user = FirebaseAuth.instance.currentUser;
+    log(user.toString());
+    if (user != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MorningView()),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const GetStartedScreen()),
+      );
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:minders/core/utils/themes/app_themes.dart';
 import 'package:minders/features/registration/splash/splash_screen.dart';
 
-import 'firebase_options.dart';
-
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
   runApp(const MindersApp());
 }
 
